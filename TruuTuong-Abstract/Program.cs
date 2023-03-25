bfe16aed4f5cff35c2dd6e849d02dbd99874cd6b
HaiNv <hainv@ldcc.vn>
2023-03-25 20:32:15 +0700

    hainv

--- TruuTuong-Abstract/Program.cs
+++ TruuTuong-Abstract/Program.cs
@@ -1,34 +1,62 @@
 ﻿using System;
 namespace Abstract {
 
-    class Product {
-        protected double Price {
-            set;
-            get;
-        }
+    // abstract class Product {
+    //     protected double Price {
+    //         set;
+    //         get;
+    //     }
 
-        public virtual void  ProductInfo() {
-            System.Console.WriteLine($"hainv {Price}");
-        }
+    //     public abstract void  ProductInfo();
 
-        public void Test() =>ProductInfo();
+    //     public void Test() =>ProductInfo();
+    // }
+
+    // class Iphone : Product {
+    //     public Iphone() => Price = 500;
+    //     // override - nap chong phuong thuc -> cho biet phuong thuc nay ghi de nen phuong thuc da khai bao o lop co so
+
+    //     public override void ProductInfo()
+    //     {
+    //         System.Console.WriteLine("Dien thoai IP");
+    //         base.ProductInfo();
+    //     }
+    // }
+
+    interface IHinhHoc {
+       public double TinhChuVi();
+
+       public double TinhDienTich();
+        
     }
 
-    class Iphone : Product {
-        public Iphone() => Price = 500;
-        // override - nap chong phuong thuc -> cho biet phuong thuc nay ghi de nen phuong thuc da khai bao o lop co so
-
-        public override void ProductInfo()
-        {
-            System.Console.WriteLine("Dien thoai IP");
-            base.ProductInfo();
+    // Trien khai giao dien
+    class HinhChuNhat : IHinhHoc
+     {
+        // khai bao
+        public HinhChuNhat(double _a, double _b) {
+            a = _a;
+            b = _b;
         }
+        public double a {
+            get;
+            set;
+        }
+        public double b {
+            get;
+            set;
+        }
+
+        public double TinhChuVi() => 2* (a + b);
+        public double TinhDienTich() => a * b;
     }
     class Program {
         static void Main(string [] args) {
-        
-            Iphone i = new Iphone();
-            i.Test();
+            // Product B = new Product();
+            // Iphone i = new Iphone();
+            // i.Test();
+            HinhChuNhat h = new HinhChuNhat(4, 9);
+            System.Console.WriteLine($"Dien tich hcn {h.TinhChuVi()} Chu vi hcn {h.TinhDienTich}");
 
         }
     }
